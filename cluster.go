36e9070653844135318b67f0e59fbf6ac12f084c
Saswat Padhi <saswat.sourav@gmail.com>
2014-01-29 07:52:39 +0530

    Addressed some suggestions in Review #1     + Better documentation and gofmt-ed     + Switched to gob for serialization of socket comms

--- cluster.go
+++ cluster.go
@@ -1,227 +1,249 @@
 package cluster
 
 import (
-    zmq "github.com/pebbe/zmq4"
+	zmq "github.com/pebbe/zmq4"
 
-    "encoding/json"
-    "io/ioutil"
+	"encoding/gob"
+	"encoding/json"
+	"io/ioutil"
 
-    "errors"
-    "fmt"
-    "io"
-    "log"
-    "os"
-    "strings"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 const (
-    BROADCAST = -1
+	// Envelope Pid for a broadcast message
+	BROADCAST = -1
 
-    BAD_JSON_CONFIG = "JSON Decode failed on the config file."
-    INVALID_SELF_ID = "The server's pid was not found in the config file."
-    IO_ERROR_CONFIG = "Unable to locate config file."
-    IP_BIND_FAILURE = "Could not bind to the specified address."
+	// Error messages
+	BAD_JSON_CONFIG = "JSON Decode failed on the config file."
+	INVALID_SELF_ID = "The server's pid was not found in the config file."
+	IO_ERROR_CONFIG = "Unable to locate config file."
+	IP_BIND_FAILURE = "Could not bind to the specified address."
 )
 
+// Envelope represents a unit message that can be exchanged between servers.
+// The Pid field represents the id of sender server for incoming messages; or
+// the id of the target server for outgoing messages.
+// The Pid can also be set to BROADCAST for an outgoing message, which would
+// indicate that the envelope should be broadcast to all servers in the cluster.
 type Envelope struct {
-    Pid   int
-    MsgId int64 //unused currently
-    Msg   interface{}
+	Pid   int
+	MsgId int64 //unused currently
+	Msg   interface{}
 }
 
-type Peer struct {
-    Pid  int
-    Addr string
+// Peer holds an id and an associated address for another server in the cluster.
+type peer struct {
+	Pid  int
+	Addr string
 }
 
+// ServerI is an interface that a Server object must provide.
 type ServerI interface {
-    Pid() int
-    Peers() []int
-    Inbox() chan *Envelope
-    Outbox() chan *Envelope
+	Pid() int
+	Peers() []int
+	Inbox() chan *Envelope
+	Outbox() chan *Envelope
 }
 
+// Server is an object that represents a single server in the cluster. This
+// object implements the ServerI interface.
 type Server struct {
-    pid            int
-    addr           string
-    sock           *zmq.Socket
-    peers          map[int]Peer
-    inbox          chan *Envelope
-    outbox         chan *Envelope
-    stop           chan bool
-    stopped        chan bool
-    release_inbox  chan bool
-    terminate_code string
+	pid            int
+	addr           string
+	sock           *zmq.Socket
+	peers          map[int]peer
+	inbox          chan *Envelope
+	outbox         chan *Envelope
+	stop           chan bool
+	stopped        chan bool
+	terminate_code string
 }
 
+// Generates a Version 4 (pseudo-random) UUID (Universally Unique Identifier).
+// This function is used to generate unique an terminate_code for a server and
+// may be used for tests.
+// TODO: Error handling!
 func GenerateUUID() string {
-    dev_rand, _ := os.Open("/dev/urandom")
-    rand_bytes := make([]byte, 16)
-    dev_rand.Read(rand_bytes)
-    dev_rand.Close()
-    return fmt.Sprintf("%x-%x-%x-%x-%x", rand_bytes[0:4], rand_bytes[4:6], rand_bytes[6:8], rand_bytes[8:10], rand_bytes[10:])
+	dev_rand, _ := os.Open("/dev/urandom")
+	rand_bytes := make([]byte, 16)
+	dev_rand.Read(rand_bytes)
+	dev_rand.Close()
+	return fmt.Sprintf("%x-%x-4%x-b%x-%x", rand_bytes[0:4], rand_bytes[4:6],
+		rand_bytes[6:8], rand_bytes[8:10], rand_bytes[10:])
 }
 
-func New(id int, config string) (*Server, error) {
-    s := new(Server)
-    s.pid = id
-    s.stop = make(chan bool)
-    s.peers = make(map[int]Peer)
-    s.inbox = make(chan *Envelope)
-    s.outbox = make(chan *Envelope)
-    s.stopped = make(chan bool)
-    s.release_inbox = make(chan bool)
-    s.terminate_code = GenerateUUID()
+// NewServer creates and returns a new Server object with the provided id and
+// the peers provided in the config file.
+// In case of an error, this function should return a nil server and a non-nil
+// error.
+func NewServer(id int, config string) (*Server, error) {
+	s := new(Server)
+	s.pid = id
+	s.stop = make(chan bool)
+	s.peers = make(map[int]peer)
+	s.inbox = make(chan *Envelope)
+	s.outbox = make(chan *Envelope)
+	s.stopped = make(chan bool)
+	s.terminate_code = GenerateUUID()
 
-    data, err := ioutil.ReadFile(config)
-    if err != nil {
-        return nil, errors.New(IO_ERROR_CONFIG + err.Error())
-    }
+	data, err := ioutil.ReadFile(config)
+	if err != nil {
+		return nil, errors.New(IO_ERROR_CONFIG + err.Error())
+	}
 
-    decoder := json.NewDecoder(strings.NewReader(string(data)))
-    for {
-        var p Peer
-        if err := decoder.Decode(&p); err == io.EOF {
-            break
-        } else if err != nil {
-            return nil, errors.New(BAD_JSON_CONFIG + err.Error())
-        } else if p.Pid == s.pid {
-            s.addr = p.Addr
-            continue
-        }
-        s.peers[p.Pid] = p
-    }
+	decoder := json.NewDecoder(bytes.NewReader(data))
+	for {
+		var p peer
+		if err := decoder.Decode(&p); err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, errors.New(BAD_JSON_CONFIG + err.Error())
+		} else if p.Pid == s.pid {
+			s.addr = p.Addr
+			continue
+		}
+		s.peers[p.Pid] = p
+	}
 
-    if len(s.addr) == 0 {
-        return nil, errors.New(INVALID_SELF_ID)
-    }
+	if len(s.addr) == 0 {
+		return nil, errors.New(INVALID_SELF_ID)
+	}
 
-    sock, _ := zmq.NewSocket(zmq.PULL)
-    if err := sock.Bind("tcp://*" + s.addr); err != nil {
-        return nil, errors.New(IP_BIND_FAILURE)
-    }
-    s.sock = sock
+	sock, _ := zmq.NewSocket(zmq.PULL)
+	if err := sock.Bind("tcp://*" + s.addr); err != nil {
+		return nil, errors.New(IP_BIND_FAILURE)
+	}
+	s.sock = sock
 
-    go s.monitorInbox()
-    go s.monitorOutbox()
+	go s.monitorInbox()
+	go s.monitorOutbox()
 
-    return s, nil
+	return s, nil
 }
 
-func (s *Server) writeToSocket(pid int, env *Envelope) {
-    sock, _ := zmq.NewSocket(zmq.PUSH)
-    defer sock.Close()
+// Sends an envelope to the peer with the provided peer id.
+func (s *Server) writeToServer(pid int, env *Envelope) {
+	sock, _ := zmq.NewSocket(zmq.PUSH)
+	defer sock.Close()
 
-    env.Pid = s.pid
-    msg, _ := json.Marshal(env)
-    if p, ok := s.peers[pid]; !ok {
-        log.Printf("[?] Dropping outgoing envelope %s -- Pid not found.\n", msg)
-        return
-    } else {
-        if err := sock.Connect("tcp://localhost" + p.Addr); err != nil {
-            log.Printf("[!] Dropping outgoing envelope %s -- Could not connect to server [%d] @ %s.\n", msg, p.Pid, p.Addr)
-            log.Printf("[!]   Error = %s.\n", err.Error())
-            return
-        }
-    }
-    sock.Send(string(msg), 0)
+	env.Pid = s.pid
+
+	msg := new(bytes.Buffer)
+	enc := gob.NewEncoder(msg)
+	enc.Encode(env)
+	if p, ok := s.peers[pid]; !ok {
+		log.Printf("[?] Dropping outgoing envelope %s -- Pid not found.\n", msg)
+		return
+	} else {
+		if err := sock.Connect("tcp://localhost" + p.Addr); err != nil {
+			log.Printf("[!] Dropping outgoing envelope %s -- Could not connect to server [%d] @ %s.\n", msg, p.Pid, p.Addr)
+			log.Printf("[!]   Error = %s.\n", err.Error())
+			return
+		}
+	}
+	sock.SendBytes(msg.Bytes(), 0)
 }
 
-func (s *Server) readFromSocket(dchan chan string) {
-    for {
-        if reply, err := s.sock.Recv(0); err != nil {
-            log.Printf("[/] Error reading from socket of server [%d]. Stopping.\n", s.pid)
-            s.sock.Close()
-            s.stopped <- true
-            return
-        } else if reply == s.terminate_code {
-            s.sock.Close()
-            s.stopped <- true
-            return
-        } else {
-            dchan <- reply
-        }
-    }
+// Reads data from the server's socket and sends them to the provided channel.
+func (s *Server) readFromServer(dchan chan []byte) {
+	for {
+		if reply, err := s.sock.RecvBytes(0); err != nil {
+			log.Printf("[/] Error reading from socket of server [%d]. Stopping.\n", s.pid)
+			s.sock.Close()
+			s.stopped <- true
+			return
+		} else if string(reply) == s.terminate_code {
+			s.sock.Close()
+			s.stopped <- true
+			return
+		} else {
+			dchan <- reply
+		}
+	}
 }
 
+// Monitors the server's inbox channel for envelopes.
 func (s *Server) monitorInbox() {
-    data_chan := make(chan string)
-    go s.readFromSocket(data_chan)
+	data_chan := make(chan []byte)
+	go s.readFromServer(data_chan)
 
-    for {
-        select {
-        case <-s.stop:
-            return
-        case data := <-data_chan:
-            var envelope Envelope
-            if err := json.Unmarshal([]byte(data), &envelope); err != nil {
-                log.Printf("[!] Dropping incoming envelope %s -- Could not unmarshal.\n", data)
-            } else {
-                s.inbox <- &envelope
-            }
-        }
-    }
+	for {
+		select {
+		case <-s.stop:
+			return
+		case data := <-data_chan:
+			var envelope Envelope
+			msg := bytes.NewBuffer(data)
+			dec := gob.NewDecoder(msg)
+			if err := dec.Decode(&envelope); err != nil {
+				log.Printf("[!] Dropping incoming envelope %s -- Could not unmarshal.\n", data)
+			} else {
+				s.inbox <- &envelope
+			}
+		}
+	}
 }
 
+// Monitors the server's outbox channel for envelopes.
 func (s *Server) monitorOutbox() {
-    for {
-        select {
-        case <-s.stop:
-            return
-        case envelope := <-s.outbox:
-            if envelope.Pid != BROADCAST {
-                go s.writeToSocket(envelope.Pid, envelope)
-            } else {
-                for p := range s.peers {
-                    go s.writeToSocket(p, envelope)
-                }
-            }
-        }
-    }
+	for {
+		select {
+		case <-s.stop:
+			return
+		case envelope := <-s.outbox:
+			if envelope.Pid != BROADCAST {
+				go s.writeToServer(envelope.Pid, envelope)
+			} else {
+				for p := range s.peers {
+					go s.writeToServer(p, envelope)
+				}
+			}
+		}
+	}
 }
 
-func (s *Server) DumpInbox() {
-    for {
-        select {
-        case <-s.release_inbox:
-            return
-        case envelope := <-s.inbox:
-            msg, _ := json.Marshal(envelope)
-            log.Printf("[>] New envelope in server [%d] inbox :: %s.\n", s.pid, msg)
-        }
-    }
-}
-
+// Stop function attempts to bring down an active server gracefully.
 func (s *Server) Stop() {
-    s.stop <- true
-    s.stop <- true
+	s.stop <- true
+	s.stop <- true
 
-    sock, _ := zmq.NewSocket(zmq.PUSH)
-    defer sock.Close()
-    if err := sock.Connect("tcp://localhost" + s.addr); err != nil {
-        log.Printf("[!] Looks like server [%d] has already closed it's socket.\n", s.pid)
-        return
-    }
-    sock.Send(s.terminate_code, 0)
-    <-s.stopped
+	sock, _ := zmq.NewSocket(zmq.PUSH)
+	defer sock.Close()
+	if err := sock.Connect("tcp://localhost" + s.addr); err != nil {
+		log.Printf("[!] Looks like server [%d] has already closed it's socket.\n", s.pid)
+		return
+	}
+
+	sock.Send(s.terminate_code, 0)
+	<-s.stopped
 }
 
+// Returns the Pid of the server in the cluster.
 func (s *Server) Pid() int {
-    return s.pid
+	return s.pid
 }
 
+// Returns the Pid's of all other servers in the same cluster as this server.
 func (s *Server) Peers() []int {
-    peers := make([]int, 0, len(s.peers))
-    for k := range s.peers {
-        peers = append(peers, k)
-    }
-    return peers
+	peers := make([]int, 0, len(s.peers))
+	for k := range s.peers {
+		peers = append(peers, k)
+	}
+	return peers
 }
 
+// Returns the inbox channel of the server.
 func (s *Server) Inbox() chan *Envelope {
-    return s.inbox
+	return s.inbox
 }
 
+// Returns the outbox channel of the server.
 func (s *Server) Outbox() chan *Envelope {
-    return s.outbox
+	return s.outbox
 }
--- cluster_test.go
+++ cluster_test.go
@@ -1,124 +1,134 @@
 package cluster
 
 import (
-    "encoding/json"
-    "io/ioutil"
-    "math/rand"
+	"encoding/json"
+	"io/ioutil"
+	"math/rand"
 
-    "bytes"
-    "io"
-    "sort"
-    "testing"
-    "time"
+	"bytes"
+	"io"
+	"sort"
+	"testing"
+	"time"
 )
 
 const (
-    CONFIG_FILE = "cluster.json"
+	CONFIG_FILE = "cluster.json"
 )
 
+// Brings up a cluster with server descriptions as specified in CONFIG_FILE.
 func ClusterSetup(t *testing.T) []*Server {
-    data, err := ioutil.ReadFile(CONFIG_FILE)
-    if err != nil {
-        t.Fatal("[X] " + IO_ERROR_CONFIG + "(" + CONFIG_FILE + ")")
-    }
+	data, err := ioutil.ReadFile(CONFIG_FILE)
+	if err != nil {
+		t.Fatal("[X] " + IO_ERROR_CONFIG + "(" + CONFIG_FILE + ")")
+	}
 
-    servers := make([]*Server, 0, bytes.Count(data, []byte{10})+1)
-    decoder := json.NewDecoder(bytes.NewReader(data))
-    for {
-        var p Peer
-        if err := decoder.Decode(&p); err == io.EOF {
-            break
-        } else if err != nil {
-            t.Error("[!] " + err.Error())
-        }
+	servers := make([]*Server, 0, bytes.Count(data, []byte{10})+1)
+	decoder := json.NewDecoder(bytes.NewReader(data))
+	for {
+		var p peer
+		if err := decoder.Decode(&p); err == io.EOF {
+			break
+		} else if err != nil {
+			t.Error("[!] " + err.Error())
+		}
 
-        t.Logf("[+] Setting up server instance [%d] @ %s.\n", p.Pid, p.Addr)
-        server, err := New(p.Pid, CONFIG_FILE)
-        if err != nil {
-            t.Error("[!] " + err.Error())
-        } else {
-            servers = append(servers, server)
-        }
-    }
+		t.Logf("[+] Setting up server instance [%d] @ %s.\n", p.Pid, p.Addr)
+		server, err := NewServer(p.Pid, CONFIG_FILE)
+		if err != nil {
+			t.Error("[!] " + err.Error())
+		} else {
+			servers = append(servers, server)
+		}
+	}
 
-    return servers
+	return servers
 }
 
+// Brings down a cluster consisting of the slice of servers provided.
 func ClusterTearDown(t *testing.T, servers []*Server) {
-    for _, server := range servers {
-        t.Logf("[-] Destroying server instance [%d] @ %s.\n", server.pid, server.addr)
-        server.Stop()
-    }
+	for _, server := range servers {
+		t.Logf("[-] Destroying server instance [%d] @ %s.\n", server.pid, server.addr)
+		server.Stop()
+	}
 }
 
+// TEST: Checks if a cluster could be brought up and down successfully.
 func Test_ClusterInitialize(t *testing.T) {
-    ClusterTearDown(t, ClusterSetup(t))
+	ClusterTearDown(t, ClusterSetup(t))
 }
 
+// TEST: Checks if the cluster configuration could be brought up and down
+//       multiple times. This might expose problems in cleanly tearing down
+//       a running cluster.
 func Test_ClusterMultiSetupDestroy(t *testing.T) {
-    ClusterTearDown(t, ClusterSetup(t))
-    ClusterTearDown(t, ClusterSetup(t))
-    ClusterTearDown(t, ClusterSetup(t))
+	ClusterTearDown(t, ClusterSetup(t))
+	ClusterTearDown(t, ClusterSetup(t))
+	ClusterTearDown(t, ClusterSetup(t))
 }
 
+// TEST: Checks if a cluster with bad configuration can be initialized.
+//       The only bad configuration we are checking now is if the server's id
+//       is missing from the cluster configuration.
 func Test_ClusterBadInitialize(t *testing.T) {
-    servers := ClusterSetup(t)
-    pids := append(servers[0].Peers(), servers[0].Pid())
-    sort.Ints(pids)
-    bad_pid := pids[len(pids)-1] + 1
-    server, err := New(bad_pid, CONFIG_FILE)
-    if (err == nil) != (server != nil) {
-        t.Errorf("[!] Inconsistent return values from Server.New()!\n", bad_pid, CONFIG_FILE)
-    } else if err == nil {
-        t.Errorf("[!] Server creation with pid %d (missing in %s) was successful!\n", bad_pid, CONFIG_FILE)
-    }
-    ClusterTearDown(t, servers)
+	servers := ClusterSetup(t)
+	pids := append(servers[0].Peers(), servers[0].Pid())
+	sort.Ints(pids)
+	bad_pid := pids[len(pids)-1] + 1
+	server, err := NewServer(bad_pid, CONFIG_FILE)
+	if (err == nil) != (server != nil) {
+		t.Errorf("[!] Inconsistent return values from Server.New()!\n", bad_pid, CONFIG_FILE)
+	} else if err == nil {
+		t.Errorf("[!] Server creation with pid %d (missing in %s) was successful!\n", bad_pid, CONFIG_FILE)
+	}
+	ClusterTearDown(t, servers)
 }
 
+// TEST: Checks if broadcast messages are correctly propagated.
 func Test_ClusterBroadcast(t *testing.T) {
-    servers := ClusterSetup(t)
+	servers := ClusterSetup(t)
 
-    rand.Seed(101)
-    env_broadcast_1_id := rand.Intn(len(servers))
-    env_expected_1 := Envelope{servers[env_broadcast_1_id].pid, 0, GenerateUUID()}
-    env_broadcast_1 := env_expected_1
-    env_broadcast_1.Pid = BROADCAST
+	rand.Seed(101)
+	env_broadcast_1_id := rand.Intn(len(servers))
+	env_expected_1 := Envelope{servers[env_broadcast_1_id].pid, 0, GenerateUUID()}
+	env_broadcast_1 := env_expected_1
+	env_broadcast_1.Pid = BROADCAST
 
-    env_broadcast_2_id := rand.Intn(len(servers))
-    env_expected_2 := Envelope{servers[env_broadcast_2_id].pid, 0, GenerateUUID()}
-    env_broadcast_2 := env_expected_2
-    env_broadcast_2.Pid = BROADCAST
+	env_broadcast_2_id := rand.Intn(len(servers))
+	env_expected_2 := Envelope{servers[env_broadcast_2_id].pid, 0, GenerateUUID()}
+	env_broadcast_2 := env_expected_2
+	env_broadcast_2.Pid = BROADCAST
 
-    servers[env_broadcast_1_id].Outbox() <- &env_broadcast_1
-    servers[env_broadcast_2_id].Outbox() <- &env_broadcast_2
+	servers[env_broadcast_1_id].Outbox() <- &env_broadcast_1
+	servers[env_broadcast_2_id].Outbox() <- &env_broadcast_2
 
-    for _, server := range servers {
-        env_1_rvcd := server.pid == env_expected_1.Pid
-        env_2_rcvd := server.pid == env_expected_2.Pid
+	for _, server := range servers {
+		env_1_rvcd := server.pid == env_expected_1.Pid
+		env_2_rcvd := server.pid == env_expected_2.Pid
 
-        for !(env_1_rvcd && env_2_rcvd) {
-            select {
-            case envelope := <-server.Inbox():
-                if *envelope == env_expected_1 {
-                    if !env_1_rvcd {
-                        env_1_rvcd = true
-                    } else {
-                        t.Errorf("[!] Broadcast envelope %s was received more than once at server [%d].\n", envelope, server.pid)
-                    }
-                } else if *envelope == env_expected_2 {
-                    if !env_2_rcvd {
-                        env_2_rcvd = true
-                    } else {
-                        t.Errorf("[!] Broadcast envelope %s was received more than once at server [%d].\n", envelope, server.pid)
-                    }
-                } else {
-                    t.Errorf("[!] Unexpected envelope %s was received at server [%d]!\n", envelope, server.pid)
-                }
-            case <-time.After(5 * time.Second):
-                t.Errorf("[!] Time out waiting for broadcast envelopes at server [%d]. Status = %s, %s.\n", server.pid, env_1_rvcd, env_2_rcvd)
-            }
-        }
-    }
+		for !(env_1_rvcd && env_2_rcvd) {
+			select {
+			case envelope := <-server.Inbox():
+				if *envelope == env_expected_1 {
+					if !env_1_rvcd {
+						env_1_rvcd = true
+					} else {
+						t.Errorf("[!] Broadcast envelope %s was received more than once at server [%d].\n", envelope, server.pid)
+					}
+				} else if *envelope == env_expected_2 {
+					if !env_2_rcvd {
+						env_2_rcvd = true
+					} else {
+						t.Errorf("[!] Broadcast envelope %s was received more than once at server [%d].\n", envelope, server.pid)
+					}
+				} else {
+					t.Errorf("[!] Unexpected envelope %s was received at server [%d]!\n", envelope, server.pid)
+				}
+			case <-time.After(5 * time.Second):
+				t.Errorf("[!] Time out waiting for broadcast envelopes at server [%d]. Status = %s, %s.\n", server.pid, env_1_rvcd, env_2_rcvd)
+			}
+		}
+	}
 
-    ClusterTearDown(t, servers)
+	ClusterTearDown(t, servers)
 }
